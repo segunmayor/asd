af72957d9bd3b8a57036177040e398c9d81aef0b
Segun Mayor <segunmayor4331@gmail.com>
2023-08-22 20:21:22 +0100

    added static build

--- asddiagnoses/settings.py
+++ asddiagnoses/settings.py
@@ -149,6 +149,11 @@ STATIC_URL = "/static/"
 
 MEDIA_URL = "/images/"
 
+STATIC_ROOT = [
+    os.path.join(BASE_DIR, 'staticfiles_build'),
+    "static"
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
