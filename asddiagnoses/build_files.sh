0b28dbbb3ee2f0c006e41cdf8e2f7b747f432a3c
Segun Mayor <segunmayor4331@gmail.com>
2023-08-22 20:12:08 +0100

    config

--- asddiagnoses/build_files.sh
+++ asddiagnoses/build_files.sh
@@ -1,5 +1,5 @@
 source ../bin/activate
-echo "BUILD START"
+echo " BUILD START"
 python3.10 -m pip3 install -r requirements.txt
 python3.10 manage.py collectstatic --noinput --clear
-echo "BUILD END"
+echo " BUILD END"
