8370ca405eac7318a16ce1cb3f67013ad50b81e1
Segun Mayor <segunmayor4331@gmail.com>
2023-08-24 15:01:53 +0100

    config

--- asddiagnoses/urls.py
+++ asddiagnoses/urls.py
@@ -43,4 +43,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('baton/', include('baton.urls')),
     # path('api-auth/', include('rest_framework.urls'))
-]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
